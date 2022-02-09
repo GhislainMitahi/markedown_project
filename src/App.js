c4c6ee9251c6760464b75f3dfd1dd2fece698225
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-02-09 20:25:47 +0100

    first  commit, v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //State variables
+    const [text, setText] = useState('Mon textarea par defaut')
+    //functions
+    const markdownToHTML = () => {
+        return { __html: marked(text) }
+    }
+
+    const changeTexHandler = (event) => {
+        setText(event.target.value); 
+
+    }
+
+    return ( <
+        div className = "App" >
+        <
+        div className = "elements" >
+
+        <
+        div className = "element" >
+        < 
+        textarea rows = "30"
+        value = { text }
+        onChange = {
+            (e) => changeTexHandler(e)
+        } > < /textarea> < /
+        div >
+
+
+        <
+        div className = "element" >
+        <
+        div dangerouslySetInnerHTML = { markdownToHTML() } >
+        <
+        /div> </
+        div>
+        <
+        /div> < /
+        div >
+    );
 }
 
 export default App;
